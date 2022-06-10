eb262d7b63bd83f8e96fe39f26bb1686645d85ee
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-10 20:16:06 +0500

    commit

--- src/Page/HeaderInput/HeaderInput.jsx
+++ src/Page/HeaderInput/HeaderInput.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 
 const HeaderInput = () =>{
     return(
         <div className="headerinput">
             <Navbar />
+            <Header />
         </div>
     )
 }
