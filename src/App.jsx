4e65396e8e130841decece7c41d4f2fae8f3d8d8
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-10 20:14:20 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import Category from './components/Category/Category';
+import Sofa from './components/Sofa/Sofa';
 import HeaderInput from './Page/HeaderInput/HeaderInput';
 
 function App() {
+
+
   return (
     <div className="App">
        <HeaderInput />
+       <Category />
+       <Sofa />
     </div>
   );
 }
