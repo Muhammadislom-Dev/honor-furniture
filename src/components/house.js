c5676020f27da1e8b993c1f5add0d339dbf3173c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-13 17:41:03 +0500

    commit

---
+++ src/components/house.js
@@ -0,0 +1,40 @@
+const obj = [
+    {
+        id: 1,
+        title: "Blog1",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://hmgroup.com/wp-content/uploads/2022/04/HM-HOME_Berlin-1024x683.jpg"
+    },
+    {
+        id: 2,
+        title: "Blog2",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://fastly.4sqi.net/img/general/600x600/2999040_mrpfS_KFTOW9qH34Fus2391PdiH6n6zPD3bvB2otsIE.jpg"
+    },
+    {
+        id: 3,
+        title: "Blog3",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://i2.wp.com/www.fashionstreet-berlin.de/wp-content/uploads/2018/10/HM-HOME-Hamburg_Living_2.jpg?ssl=1"
+    },
+    {
+        id: 4,
+        title: "Blog4",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://i2-prod.birminghammail.co.uk/incoming/article16745369.ece/ALTERNATES/s615/1_ZCR_BEM_130819hm.jpg"
+    },
+    {
+        id: 5,
+        title: "Blog5",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://fastly.4sqi.net/img/general/600x600/496431765_YN34fHN-HJsmvMLKzCOlcxNz4EM_fgHwPVt9zbwo1yA.jpg"
+    },
+    {
+        id: 6,
+        title: "Blog6",
+        text: "Lorem ipsum dolor sit amet.",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtdluutLO8ySPOBgixf85UO1nrc6Bx1b5gbZFqewlkHEWKvy2PX0yqe2oXgrq43EGiu2g&usqp=CAU"
+    },
+]
+
+export default obj;
