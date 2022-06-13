f6155613eaf114aae4103cd39ab10b0cb51ec701
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-13 17:42:05 +0500

    Initial commit

---
+++ src/components/Header/data.js
@@ -0,0 +1,26 @@
+import nbu from '../../Assets/img/furnitures.png'
+import nbus from '../../Assets/img/furnitures.png'
+import home from '../../Assets/img/chair2.png'
+
+export default [
+    [
+        {
+            img:home
+        }
+    ],
+    [
+        {
+            img:nbus
+        }
+    ],
+    [
+        {
+            img:nbu
+        }
+    ],
+    [
+        {
+            img:nbus
+        }
+    ],
+]
