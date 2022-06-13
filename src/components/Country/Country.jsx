6c7db4396242fa88452d9681e4515deebae077ac
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-13 17:41:43 +0500

    commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,36 @@
+import "./Country.scss";
+import CountryFlag from "../../Assets/img/3.png"
+
+const  Country=()=>{
+    return(
+        <section className="country">
+            <div className="container">
+                <div className="country-header">
+                    <h2 className="country-title">
+                    Biz export qiladigan davlatlar
+                    </h2>
+                </div>
+                <div className="country-footer">
+                    <ul className="country-list">
+                        <li className="country-item">
+                          <img src={CountryFlag} alt="" className="country-img" />          
+                        </li>
+                        <li className="country-item">
+                            <img src={CountryFlag} alt="" className="country-img" />          
+                        </li> <li className="country-item">
+                            <img src={CountryFlag} alt="" className="country-img" />          
+                        </li> <li className="country-item">
+                            <img src={CountryFlag} alt="" className="country-img" />          
+                        </li> <li className="country-item">
+                            <img src={CountryFlag} alt="" className="country-img" />          
+                        </li> <li className="country-item">
+                           <img src={CountryFlag} alt="" className="country-img" />          
+                        </li>
+                    </ul>
+                 </div>
+            </div>
+        </section>
+    )
+}
+
+export default Country;
