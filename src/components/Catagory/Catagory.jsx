fcfd1608157829837b536bbc7b2fc8efb96f2ed3
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-18 17:10:30 +0500

    commit

---
+++ src/components/Catagory/Catagory.jsx
@@ -0,0 +1,44 @@
+import './Catagory.scss'
+import { Link } from 'react-router-dom'
+import arrow from '../../Assets/img/arrow.png'
+
+const Catagory = () => {
+    return(
+        <div className="catagory">
+            <div className="container">
+            <h2 className="catalogy__name">Classic chairs with special design</h2>
+                 <div className="catalogy__title">
+                    <a href="#" className="catalogy__link">Catalogue   <img src={arrow} alt="" className="catalogy__img" /></a>
+                 </div>
+
+                 <ul className="catagory__list">
+                    <li className="catalogy__item">
+                        {/* <a href='#' className="catalogy__numbers">01-</a> */}
+                        <Link to='/catagory1' className="catalogy__numbers">01-</Link>
+                    </li>
+                    <li className="catalogy__item">
+                        {/* <a  href='#' className="catalogy__number">02-</a> */}
+                        <Link to='/catagory2' className="catalogy__number">02-</Link>
+                    </li>
+                    <li className="catalogy__item">
+                        {/* <a  href='#' className="catalogy__numbers">03-</a> */}
+                        <Link to='/catagory3' className="catalogy__numbers">03-</Link>
+                    </li>
+                 </ul>
+
+                 <ul className="catagory__lists">
+                    <li className="catalogy__items">
+                        <button className="catalogy__btn"></button>
+                    </li>
+                    <li className="catalogy__items">
+                        <button className="catalogy__btns"></button>
+                    </li>
+                    <li className="catalogy__items">
+                        <button className="catalogy__btns"></button>
+                    </li>
+                 </ul>
+            </div>
+        </div>
+    )
+}
+export default Catagory
