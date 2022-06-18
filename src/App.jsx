d650546e485b6a48e337c92129d65e837f5b91de
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-18 17:10:10 +0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
 import Category from './components/Category/Category';
 import Country from './components/Country/Country';
 import Footer from './components/Footer/Footer';
 import SliderBox from './components/Slider/Slider';
+
 import HeaderInput from './Page/HeaderInput/HeaderInput';
+import Catagory1 from './Page/Catagory1/Catagory1';
+import Catagory2 from './Page/Catagory2/Catagory2';
+import Catagory3 from './Page/Catagory3/Catagory3';
+
 
 function App() {
 
 
   return (
     <div className="App">
-       {/* <HeaderInput /> */}
-       {/* <Category /> */}
-       {/* <SliderBox /> */}
-       <Country />
-       <Footer />
+
+        <Switch>
+            <Route exact path="/">
+            <HeaderInput />
+            <Category />
+            <SliderBox />
+            <Country />
+            <Footer />
+          </Route>
+            <Route path='/catagory1'>
+               <Catagory1 />
+            </Route>
+            <Route  path="/catagory2">
+                <Catagory2 />
+            </Route>
+            <Route path="/catagory3">
+                <Catagory3 />
+            </Route>
+        </Switch>
     </div>
   );
 }
