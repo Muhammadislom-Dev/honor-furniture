5bacd77356b4e90aad9c1c57a404a8041edf1b4a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-18 17:11:24 +0500

    commit

---
+++ src/Page/Catagory3/Catagory3.jsx
@@ -0,0 +1,22 @@
+import Catalog from '../../components/Catalog/Catalog'
+import Category from '../../components/Category/Category'
+import Country from '../../components/Country/Country'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+import SliderBox from '../../components/Slider/Slider'
+import './Catagory3.scss'
+
+
+const Catagory3 = () => {
+    return(
+        <div className="catagory3">
+            <Navbar />
+            <Catalog />
+            <Category />
+            <SliderBox />
+            <Country />
+            <Footer />
+        </div>
+    )
+}
+export default Catagory3
