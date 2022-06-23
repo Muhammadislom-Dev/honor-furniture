6f33daa530f567fcd04cd95a7323cc48e408fb09
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-06-23 18:46:25 +0500

    add

--- src/components/Sofa/Sofa.jsx
+++ src/components/Sofa/Sofa.jsx
@@ -1,6 +1,5 @@
 import './Sofa.scss'
-import React from 'react';
-import Fade from 'react-reveal/Fade';
+
 import chair from '../../Assets/img/Tissue.png'
 import chair1 from '../../Assets/img/west.png'
 import chair2 from '../../Assets/img/wipes.png'
@@ -11,36 +10,32 @@ const Sofa = () => {
     return(
         <div className="sofa">
             <div className="container">
-                <Fade bottom>
                 <div className="sofa__page">
-                   
-                   <div className="sofa__div">
-                       <div className="sofa__list">
-                           <img src={chair} alt="" className="sofa__img" />
-                           <div className="sofa__item">
-                               <h2 className='sofa__name'>Tissues</h2>
-                               <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit</p>
-                           </div>
-                       </div>
-                       <div className="sofa__lists">
-                           <img src={chair2} alt="" className="sofa__pic" />
-                           <div className="sofa__items">
-                               <h2 className="sofa__name">Simple Wipes</h2>
-                               <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit </p>
-                           </div>
-                       </div>
-                   </div>
-                
+                    <div className="sofa__div">
+                        <div className="sofa__list">
+                            <img src={chair} alt="" className="sofa__img" />
+                            <div className="sofa__item">
+                                <h2 className='sofa__name'>Tissues</h2>
+                                <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit</p>
+                            </div>
+                        </div>
+                        <div className="sofa__lists">
+                            <img src={chair2} alt="" className="sofa__pic" />
+                            <div className="sofa__items">
+                                <h2 className="sofa__name">Simple Wipes</h2>
+                                <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit </p>
+                            </div>
+                        </div>
+                    </div>
 
-               <div className="sofa__title">
-                   <img src={chair1} alt="" className="sofa__image" />
-                   <div className="sofa__titles">
-                       <h2 className="sofa__name">Wet Wipes</h2>
-                       <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit </p>
-                   </div>
-               </div>
-           </div>
-                </Fade>
+                    <div className="sofa__title">
+                        <img src={chair1} alt="" className="sofa__image" />
+                        <div className="sofa__titles">
+                            <h2 className="sofa__name">Wet Wipes</h2>
+                            <p className="sofa__text">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit </p>
+                        </div>
+                    </div>
+                </div>
 
                 <div className="sofa__box">
                     <div className="sofa__left">
